dac99b1b4990a17f4f88c94ed11c9fde759b1db1
Oleksandr Sheremet <o.sheremet@viseven.com>
2025-05-06 13:29:41 +0300

    update(todo): add todo list

--- vue3-firebase-todo/src/App.vue
+++ vue3-firebase-todo/src/App.vue
@@ -1,47 +1,117 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="weather">
+    <div class="container">
+      <div class="card">
+        <input
+          class="card__input"
+          type="text"
+          placeholder="input city"
+          v-model="searchQuery"
+          @keyup="weatherSearch"
+        />
+        <button class="search" @click="weatherSearch">Search</button>
+      </div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <div class="card-load" v-if="loading">Loading</div>
+
+      <div class="card-info" v-show="!error && location && temperature != 0 && description">
+        <div class="card" v-show="error">Error</div>
+        <div class="card-info__text">
+          <p class="city">{{ location }}</p>
+          <p class="temp">{{ temperature }} C</p>
+          <p class="city">{{ description }}</p>
+        </div>
+      </div>
     </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="weather-bg">
+      <div class="weather-bg">
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+      </div>
+    </div>
+  </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      location: '',
+      temperature: 0,
+      description: '',
+      loading: false,
+      error: false,
+      searchQuery: '',
+    }
+  },
+  methods: {
+    weatherSearch() {
+      this.loading = true
+      this.error = false
+      fetch(
+        `http://api.weatherapi.com/v1/current.json?key=8b60d3e7b347463bab782028250605&q=${this.searchQuery}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          this.loading = false
+          this.location = data.location.name;
+          this.temperature = data.current.temp_c;
+          (this.description = data.current.condition.text), this.resetSearchQuery()
+        })
+        .catch((error) => {(this.loading = false), (this.error = true), console.error(error)})
+    },
+    resetSearchQuery() {},
+  },
+}
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+@import url('https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,200..800;1,200..800&family=League+Spartan:wght@100..900&display=swap');
+
+:root {
+  --accent: #12c0dd;
+  --accent-rgb: 18, 192, 221;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+body {
+  font-family: 'Karla', sans-serif;
+  height: 100%;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+* {
+  margin: 0;
+  padding: 0;
+}
+body,
+html {
+  height: 100vh;
+}
+input,
+button {
+  font-family: 'Karla', sans-serif;
+}
+#app {
+  height: 100%;
+}
+.weather {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  overflow: hidden;
+}
+.container {
+  width: 100%;
+  max-width: 1000px;
+  margin: 0 auto;
+  padding-left: 15px;
+  padding-right: 15px;
+  display: grid;
+  grid-template-columns: 1fr 100px 200px;
+  gap: 20px;
+  box-sizing: border-box;
 }
 </style>
